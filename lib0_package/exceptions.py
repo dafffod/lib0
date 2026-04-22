1351db69325b6dce7f5cb90a78295c7840ff231e
NotDafffodil <alexanderbodnar8@gmail.com>
2026-04-22 17:13:50 +0200

    Create exceptions.py

---
+++ lib0_package/exceptions.py
@@ -0,0 +1,19 @@
+from types import NoneType
+import inspect
+import traceback
+
+
+class ExpliciteTypeError(Exception):
+    def  __init__(self, message: str) -> None:
+        self.message = message
+        super().__init__(self.message)
+
+class TypeLockedError(Exception):
+    def __init__(self, message: str) -> None:
+         self.message = message
+         super().__init__(self.message)
+
+class Lib0Error(Exception):
+    def __init__(self, message: str) -> None:
+        self.message = message
+        super().__init__(self.message)
