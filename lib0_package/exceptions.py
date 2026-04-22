c327a568b826290919b9fa9d3aec59e9715d375d
NotDafffodil <alexanderbodnar8@gmail.com>
2026-04-22 18:52:31 +0200

    Update exceptions.py
    These changes apply to the previous update as well
    Added new more understandable error messages to improve debugging.

--- lib0_package/exceptions.py
+++ lib0_package/exceptions.py
@@ -13,6 +13,11 @@ class TypeLockedError(Exception):
          self.message = message
          super().__init__(self.message)
 
+class ConstantAssignmentError(Exception):
+    def __init__(self, message: str) -> None:
+         self.message = message
+         super().__init__(self.message)
+
 class Lib0Error(Exception):
     def __init__(self, message: str) -> None:
         self.message = message
