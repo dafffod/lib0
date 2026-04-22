107350e37369edc21e775573fd774c1428352bea
NotDafffodil <alexanderbodnar8@gmail.com>
2026-04-22 18:53:51 +0200

    Update __init__.py
    Transformed lib0 into a package instead of a simple script.

--- lib0_package/__init__.py
+++ lib0_package/__init__.py
@@ -1,6 +1,6 @@
 from .lib0 import Lib0, dict2lib0, lib02dict
-from .explicit_types import TypedVar, SetExplicitTypes
+from .explicit_types import TypedVar, ConstVars, SetExplicitTypes
 print("Working with lib0 package...")
 __author__ = "dafffod"
 __version__ = "2.0 Development"
-__all__ = ["Lib0", "dict2lib0", "lib02dict", "TypedVar", "SetExplicitTypes"]
+__all__ = ["Lib0", "dict2lib0", "lib02dict", "TypedVar", "SetExplicitTypes", "ConstVars"]
